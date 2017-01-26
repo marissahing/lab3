45dfbc42f7de26813fa80ab81df4281c71494246
Marissa Hing <mnhing@ucsd.edu>
2017-01-25 23:39:09 -0800

    Commit it

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,28 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Hi there friend.");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("#project1").click(projectClick);
 }
+function projectClick(e) {
+	e.preventDefault();
+	
+	var containingProject = $(this).closest(".project");
+	var description = $(containingProject).find(".project-description");
+
+	if (description.length == 0) {
+		containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+	}
+	else {
+		$("#project1").fadeOut();
+		//description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+	}
+}
+
+	
